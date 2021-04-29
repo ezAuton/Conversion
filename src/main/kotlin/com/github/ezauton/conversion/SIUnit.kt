70a2abad1a60a0ca8c080afa7da7fce11086f7b2
Andrew Gazelka <andrew.gazelka@gmail.com>
2021-04-29 16:35:54 -0500

    add equals

--- src/main/kotlin/com/github/ezauton/conversion/SIUnit.kt
+++ src/main/kotlin/com/github/ezauton/conversion/SIUnit.kt
@@ -32,6 +32,7 @@ abstract class SIUnit<T : SIUnit<T>> : Comparable<SIUnit<T>> {
   }
 
 
+
   operator fun minus(other: T) = of(value - other.value, other::class)
   operator fun plus(other: T) = of(value + other.value, other::class)
   operator fun div(other: T) = value / other.value
@@ -61,6 +62,21 @@ abstract class SIUnit<T : SIUnit<T>> : Comparable<SIUnit<T>> {
   val isInvalid get() = this.isNaN
 
   operator fun unaryMinus() = this * (-1)
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as SIUnit<*>
+
+    if (value != other.value) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    return value.hashCode()
+  }
 }
 
 operator fun <T : SIUnit<T>> Number.times(unit: T): T = SIUnit.of(unit.value * toDouble(), unit::class)
